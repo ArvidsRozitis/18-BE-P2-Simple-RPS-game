816a73006353a893e0c7f25bca88a7e55a170c42
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-02-15 23:17:11 +0200

    logic done, nov ve need to work on validation

--- src/server.ts
+++ src/server.ts
@@ -8,22 +8,50 @@ const app = express();
 
 app.use(bodyparser.json());
 app.use(cors({ origin: "*" }));
+app.use("/locales", express.static("locales"));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Application works!");
 });
-
-app.use("/locales", express.static("locales"));
-
-app.listen(3004, () => {
-  console.log("Application started on port 3004!");
-});
-
+// *** get all scores ***
 app.get("/scores", (req: Request, res: Response) => {
   pool.query("SELECT * FROM user_scores", (error, results) => {
     if (error) throw error;
     res.send(results);
   });
+});
+
+// *** post new user ***
+app.post("/scores/post", (req: Request, res: Response) => {
+  const { playerName, roundsWon, roundsLost, gamesWon } = req.body;
+  pool.query(
+    `INSERT INTO user_scores (playerName, roundsWon, roundsLost, gamesWon) VALUES (?, ?, ?, ?)`,
+    [playerName, roundsWon, roundsLost, gamesWon],
+    (error, results) => {
+      if (error) {
+        res.send({ error: "An error occurred while adding the score." });
+        return;
+      }
+      res.send(results);
+    }
+  );
+});
+
+//*** update user ***/
+app.put("/scores/put/:id", (req: Request, res: Response) => {
+  const scoreID = req.params.id;
+  const { roundsWon, roundsLost, gamesWon } = req.body;
+  pool.query(
+    `UPDATE user_scores SET roundsWon = ?, roundsLost = ?, gamesWon = ? WHERE id = ?`,
+    [roundsWon, roundsLost, gamesWon, scoreID],
+    (error) => {
+      if (error) {
+        res.send({ error: "An error occurred while updating the score." });
+        return;
+      }
+      res.send({ message: "Score updated successfully." });
+    }
+  );
 });
 
 const pool = mysql.createPool({
@@ -33,4 +61,8 @@ const pool = mysql.createPool({
   password: "123456",
   database: "rockPaperScissors",
   port: 3306,
+});
+
+app.listen(3004, () => {
+  console.log("Application started on port 3004!");
 });
