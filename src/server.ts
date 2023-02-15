3456dd2e85dda9ee7a932f403db167ad442dc4a4
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-02-15 20:15:44 +0200

    connected to server and, get scores done

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Request, Response } from "express";
 import bodyparser from "body-parser";
 import cors from "cors";
+import mysql from "mysql2";
 
 const app = express();
 
@@ -12,8 +13,24 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Application works!");
 });
 
+app.use("/locales", express.static("locales"));
+
 app.listen(3004, () => {
   console.log("Application started on port 3004!");
 });
 
-app.use("./locales", express.static("locales"));
+app.get("/scores", (req: Request, res: Response) => {
+  pool.query("SELECT * FROM user_scores", (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "rockPaperScissors",
+  port: 3306,
+});
