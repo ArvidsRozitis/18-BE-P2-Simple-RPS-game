0dfd86e29ab26614f6f5378e1ede338a3b667b97
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-02-16 13:47:46 +0200

    uploading docker files

---
+++ init/01.sql
@@ -0,0 +1,14 @@
+USE rockPaperScissors;
+
+CREATE TABLE user_scores (
+    id int,
+    playerName VARCHAR(255),
+    roundsWon int,
+    roundsLost int,
+    gamesWon int
+);
+
+INSERT INTO user_scores(id, playerName, roundsWon, roundsLost, gamesWon) VALUES
+(1,'EENA',9,14,1),
+(2,'Amirus',15,6,3),
+(3,'Jēkabs',8,7,1);
